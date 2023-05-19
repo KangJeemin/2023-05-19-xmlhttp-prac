8ffd4f8bd09c8521d84a3f2c4551d361272ea355
KangJeemin <wlals1234560@gmail.com>
2023-05-19 11:09:06 +0900

    왜 서버 url이 이동하지도 않았는데 post 전송이 되는걸까?

---
+++ server.js
@@ -0,0 +1,26 @@
+import http from 'http'
+import fs from 'fs'
+
+const server = http.createServer((req, res) => {
+  if (req.method === 'POST' && req.url === '/submit') {
+    let data = '';
+    req.on('data', chunk => {
+      data += chunk;
+    });
+    req.on('end', () => {
+      // 수신한 데이터 처리
+      console.log('Received data(server):', data);
+
+      // 클라이언트로 응답 전송
+      res.writeHead(200, { 'Content-Type': 'text/plain' });
+      res.end('Data received successfully!');
+    });
+  } else {
+    res.statusCode = 404;
+    res.end();
+  }
+});
+
+server.listen(3000, () => {
+  console.log('서버가동 http://localhost:3000');
+});
